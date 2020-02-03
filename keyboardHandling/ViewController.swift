d4cd5b02aae925795d37c610bc6aa7d4497bb4a8
Ahad Islam <ahad.islam624@gmail.com>
2020-02-03 10:54:03 -0500

    added placeholder image, added outlets to view controller added functions for notification center handling

--- keyboardHandling/ViewController.swift
+++ keyboardHandling/ViewController.swift
@@ -10,9 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pursuitLogo: UIImageView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var pursuitLogoCenterYConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    private func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    private func unregisterForKeyboardNotifications() {
+        
+    }
+    
+    @objc private func keyboardWillShow(_ notification: NSNotification) {
+        print("keyboardWillShow")
+        print(notification.userInfo)
+        
+    }
+    
+    @objc private func keyboardWillHide(_ notification: NSNotification) {
+        
     }
 
 
